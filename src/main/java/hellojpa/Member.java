045a710695585026b1d8fa3d34bfe63bbd796d7b
frostnight <frostnightsong@gmail.com>
2021-11-27 17:25:13 +0900

    양방향 연관관계 주인 설정

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,9 +2,12 @@ package hellojpa;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.TableGenerator;
 
@@ -18,7 +21,35 @@ public class Member {
 	@Column(name = "USERNAME")
 	private String username;
 
-	@Column(name = "TEAM_ID")
-	private Long teamId;
+	// @Column(name = "TEAM_ID")
+	// private Long teamId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "TEAM_ID")
+	private Team team;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
 }
 
