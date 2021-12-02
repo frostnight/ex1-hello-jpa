c1bbe755e95053d726680fbfb38a2ba0a6e25cc7
frostnight <frostnightsong@gmail.com>
2021-12-03 01:33:09 +0900

    실전예제2

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -5,6 +5,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 public class JpaMain {
 	public static void main(String[] args){
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -15,6 +18,14 @@ public class JpaMain {
 		tx.begin();
 
 		try{
+
+			Order order = new Order();
+			em.persist(order);
+			//order.addOrderItem(new OrderItem());
+			OrderItem orderItem = new OrderItem();
+			orderItem.setOrder(order);
+
+			em.persist(orderItem);
 
 			tx.commit();
 		} catch(Exception e){
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,9 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,7 +23,12 @@ public class Order {
 	@Column(name = "ORDER_ID")
 	private Long id;
 
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_ID")
 	private Member member;
+
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> orderItems = new ArrayList<>();
 
 	private LocalDateTime orderDate;
 
@@ -55,5 +65,10 @@ public class Order {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
 	}
 }
