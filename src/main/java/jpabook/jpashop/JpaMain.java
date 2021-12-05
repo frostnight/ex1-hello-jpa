1b8b2e2b0e2e6d285b4ef204e2e7b08feb3096de
frostnight <frostnightsong@gmail.com>
2021-12-05 16:50:16 +0900

    실전예제3

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -19,14 +19,6 @@ public class JpaMain {
 
 		try{
 
-			Order order = new Order();
-			em.persist(order);
-			//order.addOrderItem(new OrderItem());
-			OrderItem orderItem = new OrderItem();
-			orderItem.setOrder(order);
-
-			em.persist(orderItem);
-
 			tx.commit();
 		} catch(Exception e){
 			tx.rollback();
