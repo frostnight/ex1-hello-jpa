69f5a599fd854d1eee1d3866c8e863b4e9d2f821
frostnight <frostnightsong@gmail.com>
2021-12-10 18:09:58 +0900

    프록시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,12 +1,11 @@
 package hellojpa;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import org.hibernate.Hibernate;
 
 public class JpaMain {
 	public static void main(String[] args){
@@ -19,14 +18,16 @@ public class JpaMain {
 
 		try{
 
-			Member member = new Member();
-			member.setUsername("user1");
-			em.persist(member);
+			Member member1 = new Member();
+			member1.setUsername("member1");
+			em.persist(member1);
 
 			em.flush();
 			em.clear();
 
-
+			Member refMember = em.getReference(Member.class, member1.getId());
+			System.out.println("refMember = " + refMember.getClass());
+			Hibernate.initialize(refMember);
 
 			tx.commit();
 		} catch(Exception e){
@@ -35,5 +36,17 @@ public class JpaMain {
 			em.close();
 			emf.close();
 		}
+	}
+
+	private static void printMember(Member member){
+		System.out.println("member = " + member);
+	}
+
+	private static void printMemberAndTeam(Member member) {
+		String username = member.getUsername();
+		System.out.println("username = " + username);
+
+		Team team = member.getTeam();
+		System.out.println("team = " + team.getName());
 	}
 }
