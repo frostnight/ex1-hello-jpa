116e181cf8316a73901325c5dac7e910b696e1d6
frostnight <frostnightsong@gmail.com>
2021-12-10 16:03:08 +0900

    실전예제4

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -18,6 +19,12 @@ public class JpaMain {
 		tx.begin();
 
 		try{
+
+			Book book = new Book();
+			book.setName("JPA");
+			book.setAuthor("홍길동");
+
+			em.persist(book);
 
 			tx.commit();
 		} catch(Exception e){
--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -13,7 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
-public class Category {
+public class Category extends BaseEntity{
 
 	@Id @GeneratedValue
 	private Long id;
@@ -29,8 +29,8 @@ public class Category {
 
 	@ManyToMany
 	@JoinTable(name = "CATEGORY_ITEM",
-			joinColumns = @JoinColumn(name = "CATEGORY_ID"),
-			inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
+		joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+		inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
 	)
 	private List<Item> items = new ArrayList<>();
 }
