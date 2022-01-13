6cf5744603b2c1908bbf835d800ec40c8091a017
frostnight <frostnightsong@gmail.com>
2022-01-14 01:25:27 +0900

    경로 표현식

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,5 +1,6 @@
 package jpql;
 
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -20,24 +21,28 @@ public class JpaMain {
 
 		try{
 
+			Team team = new Team();
+			em.persist(team);
+
 			Member member1 = new Member();
 			member1.setUsername("관리자1");
+			member1.setTeam(team);
 			em.persist(member1);
 
 			Member member2 = new Member();
 			member2.setUsername("관리자2");
+			member2.setTeam(team);
 			em.persist(member2);
 
 			em.flush();
 			em.clear();
 
-			String query = "select locate('de', 'abcdegf') From Member m";
+			String query = "select m From Team t join t.members m";
 
-			List<Integer> resultList = em.createQuery(query, Integer.class).getResultList();
+			List<Collection> result = em.createQuery(query, Collection.class)
+				.getResultList();
 
-			for (Integer s : resultList) {
-				System.out.println("s = " + s);
-			}
+			System.out.println("result = " + result);
 
 			tx.commit();
 		} catch(Exception e){
